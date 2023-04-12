ea39e7d50ae4141a87a05b021bfc039de717e045
Peng Xiao <pengxiao@outlook.com>
2023-04-12 18:37:10 +0800

    feat: add a secret route for route auth callback

---
+++ pages/client/auth-callback.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+.page.redirect-container
+  .hint Redirecting to your AFFiNE Client App...
+  button(@click="openAffineClient") Click here to try again
+</template>
+
+<script>
+export default {
+  name: "ClientAuthRedirect",
+  mounted() {
+    this.openAffineClient();
+  },
+  methods: {
+    openAffineClient() {
+      if (typeof window !== "undefined") {
+        // do we need to concern if it is secure?
+        window.location.href =
+          "affine://auth-callback?" + window.location.search;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="stylus">
+.page.redirect-container
+  min-height: 500px
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+
+.hint
+  margin-bottom: 20px
+
+</style>
