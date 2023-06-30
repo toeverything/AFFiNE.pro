711d41eefd7fffd5d5f044ccdec6fa22b65fb8fd
SuneBear <hi@sunebear.com>
2023-06-30 20:37:06 +0800

    FEATURE(cookie-policy): Added cookie banner & policy page

---
+++ pages/cookie-policy.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.page.page-cookie-policy
+  single-article
+    content-doc(path="/cookie-policy")
+</template>
+
+<script lang="ts" setup>
+useHead({
+  title: 'Cookie Policy'
+})
+</script>
+
+<style lang="stylus">
+.page.page-cookie-policy
+  null
+</style>
