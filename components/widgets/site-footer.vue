bada909c0ac984af26add421e1fec79640006945
Sune Bear <hi@sunebear.com>
2023-11-10 11:56:40 +0800

    ISSUE(v3): Fixed about link on footer

--- components/widgets/site-footer.vue
+++ components/widgets/site-footer.vue
@@ -35,7 +35,7 @@
         .nav-list
           .list-name {{ $t('resources') }}
           nuxt-link.nav-item.flex( to="/pricing" ) {{ $t('price') }}
-          nuxt-link.nav-item.flex( to="/abous-us" ) {{ $t('aboutUs') }}
+          nuxt-link.nav-item.flex( to="/about-us" ) {{ $t('aboutUs') }}
           nuxt-link.nav-item.flex( to="/terms" ) {{ $t('terms') }}
           nuxt-link.nav-item.flex( to="/privacy" ) {{ $t('privacy') }}
           nuxt-link.nav-item.flex( :to="PATH.AFFiNE_COMMUNITY" target="_blank" rel="nofollow") {{ $t('community') }}
