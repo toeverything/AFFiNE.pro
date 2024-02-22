3d4adad93c585d651ecd8cd7f6074964ff2ca43a
ShortCipher5 <me@shortcipher.me>
2024-02-21 22:17:18 -0800

    Update en-US.js

--- locales/en-US.js
+++ locales/en-US.js
@@ -222,8 +222,8 @@ export default {
     itemMaxMembers: "Number of members per Workspace ≤ {0}",
     pro: "Pro",
     proAction: "Buy Pro",
-    contactSalesTeam: "Book a demo",
-    contactSalesEnterprise: "Book a demo",
+    contactSalesTeam: "Book a Demo",
+    contactSalesEnterprise: "Book a Demo",
     team: "Team",
     teamItemA:
       "Best team workspace for collaboration and knowledge distilling.",
