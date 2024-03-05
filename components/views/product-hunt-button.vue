95b08642c6f8727b8659af05764f8af918300317
SuneBear <hi@sunebear.com>
2024-03-05 19:20:06 +0800

    Updated ph button

--- components/views/product-hunt-button.vue
+++ components/views/product-hunt-button.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="product-hunt-button">
-    <a href="https://www.producthunt.com/posts/affine-3?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-affine&#0045;3" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=440671&theme=light" alt="AFFiNE - One&#0032;app&#0032;for&#0032;all&#0032;&#0045;&#0032;Where&#0032;Notion&#0032;meets&#0032;Miro | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+    <a href="https://www.producthunt.com/posts/affine-3?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-affine&#0045;3" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=440671&theme=light&period=weekly" alt="AFFiNE - Docs&#0044;&#0032;whiteboards&#0044;&#0032;and&#0032;databases&#0032;together&#0032;in&#0032;one&#0032;workspace | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
   </div>
 </template>
