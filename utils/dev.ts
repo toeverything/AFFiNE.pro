29a624baadfd03c6e5bf1a7ce4228099cb390e09
SuneBear <hi@sunebear.com>
2024-04-23 21:02:59 +0800

    Support env for browser client

--- utils/dev.ts
+++ utils/dev.ts
@@ -1,3 +1,10 @@
 export let __DEBUG__ = process.env.NUXT_PUBLIC_ENV === 'development'
 export let __STAGING__ = process.env.NUXT_PUBLIC_ENV === 'staging'
 export let __PROD__ = !__DEBUG__ && process.env.NUXT_PUBLIC_ENV === 'production'
+
+if (process.client) {
+  // @ts-ignore
+  const config = window.__NUXT__.config
+  const isStaging = config.public.ENV === 'staging'
+  __STAGING__ = isStaging
+}
