9ab95b0cdd92b102918721d125f6e0ff1be1b3ed
SuneBear <hi@sunebear.com>
2024-05-06 11:10:03 +0800

    Updated ph badges

--- components/views/product-hunt-button.vue
+++ components/views/product-hunt-button.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="product-hunt-button">
-    <a href="https://www.producthunt.com/posts/affine-3?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-affine&#0045;3" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=440671&theme=light&period=weekly" alt="AFFiNE - Docs&#0044;&#0032;whiteboards&#0044;&#0032;and&#0032;databases&#0032;together&#0032;in&#0032;one&#0032;workspace | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+    <a href="https://www.producthunt.com/posts/affine-ai?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-affine&#0045;ai" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=453897&theme=light" alt="AFFiNE&#0032;AI - Your&#0032;AI&#0032;partner&#0032;that&#0032;helps&#0032;you&#0032;write&#0044;&#0032;draw&#0032;&#0038;&#0032;present&#0032;better | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
   </div>
 </template>
